f39b8e5a3350d07e2907315dce15a644cfdb97cd
William Casarin <jb55@jb55.com>
2023-07-03 10:28:25 -0700

    docs: some initial rustdocs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
-pub mod parser;
+//! This crate is a fast, zero-copy [nostr] content parser. What is a
+//! [nostr] content parser? There can be many elements within the body of
+//! a note on [nostr], and to render them properly they need to be parsed
+//! out. shatter will find all the locations of the elements of interest
+//! and mark them so that can pick them out and render them properly.
+//!
+//! [nostr]: https://github.com/nostr-protocol/nostr
 pub mod shard;
